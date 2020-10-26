fd1467b1871578fd36836bf67aaa6a2fc94f4235
f19ba073 <64409053+f19ba073@users.noreply.github.com>
2020-10-26 14:00:46 +0900

    Plan b (#1)
    * クリックイベントを編集しました
    
    * クリックイベントを編集しました
    
    正解したカードを再度クリックしたときにめくられないように処理を追加しました
    
    * クリックイベントを編集しました

--- src/application/SinController.java
+++ src/application/SinController.java
@@ -1,13 +1,44 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
-public class SinController {
+public class SinController implements EventHandler<ActionEvent>{
+	private static int clickedValue = 0;
+	private static Button clickedButton;
+	private static Button wrongButton;
+
+	private final int number;
+
+	private final int CARD_VALUE = 24;
+	private final int GRID_ROW_SIZE = 3;
+	private final int GRID_COLUMN_SIZE = 8;
+	private final int CARD_HEIGHT = 90;
+	private final int CARD_WEIGHT = 60;
+
+	private List<Button> buttonList = new ArrayList<>();
+	private GridPane grid;
+
+	private SinController(int number) {
+		this.number = number;
+	}
+
+	public SinController() {
+		this(-1);
+	}
 	@FXML
     private AnchorPane base_pane;
 
@@ -20,10 +51,73 @@ public class SinController {
     @FXML
     void initialize() {
     	assert base_pane != null : "fx:id=\"base_pane\" was not injected: check your FXML file 'Sin.fxml'.";
-    	CreateCardsList ccl = new CreateCardsList();
-    	GridPane grid = ccl.getGrid();
+    	grid = new GridPane();
+		int number = 0;
+		for(int i = 0;i < CARD_VALUE; i++) {
+			if(i % 2 == 0) {
+				number++;
+			}
+
+			Button button = new Button("");
+			button.setOnAction(new SinController(number));
+			button.setPrefSize(CARD_WEIGHT, CARD_HEIGHT);
+			button.setFont(new Font(30));
+			button.setPadding(new Insets(0,0,0,0));
+			buttonList.add(button);
+		}
+		Collections.shuffle(buttonList);
+
+		for(int i = 0; i < GRID_ROW_SIZE;  i++) {
+			for(int j = 0; j < GRID_COLUMN_SIZE; j++) {
+				grid.add(buttonList.get(i * GRID_COLUMN_SIZE + j), j, i);
+			}
+		}
     	AnchorPane.setBottomAnchor(grid, 20.0);
     	AnchorPane.setRightAnchor(grid, 30.0);
         base_pane.getChildren().add(grid);
+    }
+
+    @Override
+	public void handle(ActionEvent event) {
+    	Button button = (Button)event.getSource();
+
+    	//カードの色が赤（正解状態）だった場合
+    	if(button.getTextFill().equals(Color.RED)) {
+    		return;
+    	}
+
+    	//カードが表向きだった場合
+    	if(!button.getText().isEmpty()) {
+    		if(clickedValue == 1) {
+    			return;
+    		}else {
+    			button.setText("");
+        		clickedValue--;
+        		return;
+    		}
+    	}
+
+    	if(clickedValue >= 2) {
+    		wrongButton.setText("");
+    		clickedButton.setText("");
+    		clickedValue = 0;
+    	}
+
+    	//カードが裏向きになっている場合
+    	button.setText(String.valueOf(this.number));
+
+    	if(clickedValue == 0) {
+    		clickedButton = button;
+    		clickedValue++;
+    	}else {
+    		if(button.getText().equals(clickedButton.getText())) {
+    			button.setTextFill(Color.RED);
+    			clickedButton.setTextFill(Color.RED);
+    			clickedValue = 0;
+    		}else {
+    			wrongButton = button;
+    			clickedValue++;
+    		}
+    	}
     }
 }
