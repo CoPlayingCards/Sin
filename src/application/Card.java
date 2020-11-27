0dad28f67f44a3caa75f7c061afaee496bc8f85b
TakumiMiyamoto38 <f19ba073@gmail.com>
2020-11-27 14:11:50 +0900

    コメントを追加しました

--- src/application/Card.java
+++ src/application/Card.java
@@ -18,6 +18,8 @@ public class Card  extends Label{
 	public Card(int number) {
 		super("" + number);
 		this.number = number;
+
+		//カードの大きさ、色等の設定
 		setPrefSize(CARD_WEIGHT, CARD_HEIGHT);
 		setFont(new Font(30));
 		setPadding(new Insets(0,0,0,0));
@@ -25,10 +27,12 @@ public class Card  extends Label{
 		setAlignment(Pos.CENTER);
 	}
 
+	//ブラックジャックのルールの都合上、10以上は全て10として返す
 	public int getNumber() {
 		return number > 10 ? 10 : number;
 	}
 
+	//カード裏返し
 	public void turnItOver() {
 		if(getText().isEmpty()) {
 			setText("" + number);
